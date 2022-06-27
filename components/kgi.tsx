79b1355f79eaa3cfabb1075895278eff8bd61f30
taipon55 <git config --global user.email xxxxxx@gmail.com>
2022-06-27 16:08:01 +0900

    目標と現状が出力されるように修正

--- components/kgi.tsx
+++ components/kgi.tsx
@@ -1,7 +1,19 @@
-import { Typography, Box, TextField, Button } from "@mui/material";
-import React from "react";
+import { Typography, Box, TextField, Button, Grid } from "@mui/material";
+import React, { useState } from "react";
 
-function kgi() {
+function Kgi() {
+  const [targetkgi, setTargetkgi] = useState("");
+  const [nowkgi, setNowkgi] = useState("");
+  const [addTarget, setAddtarget] = useState("");
+  const [addNow, setAddnow] = useState("");
+
+  const onClickAddText = () => {
+    setAddtarget(targetkgi);
+    setAddnow(nowkgi);
+    setTargetkgi("");
+    setNowkgi("");
+  };
+
   return (
     <>
       <Typography variant="h5" sx={{ mt: 10, ml: 10 }}>
@@ -13,19 +25,42 @@ function kgi() {
           label="目標"
           variant="standard"
           sx={{ ml: 10 }}
+          type="text"
+          value={targetkgi}
+          onChange={(event) => setTargetkgi(event.target.value)}
         />
         <TextField
           id="standard-basic"
           label="現状"
           variant="standard"
           sx={{ ml: 10 }}
+          type="text"
+          value={nowkgi}
+          onChange={(event) => setNowkgi(event.target.value)}
         />
-        <Button variant="contained" sx={{ mt: 1, ml: 5 }}>
+        <Button
+          variant="contained"
+          sx={{ mt: 1, ml: 5 }}
+          onClick={onClickAddText}
+        >
           追加
         </Button>
       </Box>
+
+      <Grid container spacing={1}>
+        <Grid item xs={5}>
+          <Typography variant="h5" sx={{ mt: 10, ml: 10 }}>
+            目標: {addTarget}
+          </Typography>
+        </Grid>
+        <Grid item xs={5}>
+          <Typography variant="h5" sx={{ mt: 10, ml: 10 }}>
+            現状: {addNow}
+          </Typography>
+        </Grid>
+      </Grid>
     </>
   );
 }
 
-export default kgi;
+export default Kgi;
